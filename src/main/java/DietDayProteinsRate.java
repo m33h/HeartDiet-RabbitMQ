8b295a6af9d5acbcdb400bb9e087fed5eefdb9cf
Michał Tarsa <mtarsa17@gmail.com>
2019-06-02 18:29:34 +0200

    Add DietDayProteinsRate callback

---
+++ src/main/java/DietDayProteinsRate.java
@@ -0,0 +1,45 @@
+
+import heart.Callback;
+import heart.Debug;
+import heart.WorkingMemory;
+import heart.alsvfd.SimpleNumeric;
+import heart.exceptions.AttributeNotRegisteredException;
+import heart.exceptions.NotInTheDomainException;
+import heart.xtt.Attribute;
+
+public class DietDayProteinsRate implements Callback{
+    public void execute(Attribute subject, WorkingMemory wmm) {
+        System.out.println("Executing DietDayProteinsRate callback");
+
+        String dietGoal = wmm.getAttributeValue("diet_goal").toString();
+        double mass = Double.valueOf(wmm.getAttributeValue("mass").toString());
+        double proteins = Double.valueOf(wmm.getAttributeValue("proteins").toString());
+
+        double proteinsDemand;
+        if(dietGoal == "reduce"){
+            proteinsDemand = mass * 1.0;
+        }
+        else if(dietGoal == "maintain") {
+            proteinsDemand = mass * 1.5;
+        }
+        else {
+            proteinsDemand = mass * 2.0;
+        }
+
+        double rateArg = (proteins * Math.PI) / (2 * proteinsDemand);
+        double rateExpValue = 20;
+        double rateValue = 100 * Math.pow(Math.sin(rateArg), rateExpValue);
+
+        try {
+            wmm.setAttributeValue(subject,new SimpleNumeric(rateValue),false);
+        } catch (AttributeNotRegisteredException e) {
+            Debug.debug("CALLBACK",
+                    Debug.Level.WARNING,
+                    "Callback failed to set value of"+subject.getName()+", as the attribute is not registered in the Working Memory.");
+        } catch (NotInTheDomainException e) {
+            Debug.debug("CALLBACK",
+                    Debug.Level.WARNING,
+                    "Callback failed to set value of"+subject.getName()+", as the obtained value was not in the domain of attribute.");
+        }
+    }
+}
